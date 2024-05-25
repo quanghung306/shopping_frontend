0d1cb91bc793ecfa8c6bbd91944ac6bfa3420bb9
Khánh Vũ <152385937+kjnhs@users.noreply.github.com>
2024-05-25 21:15:05 +0700

    hungngu

--- src/layouts/sidebar/MenuSidebar.jsx
+++ src/layouts/sidebar/MenuSidebar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./MenuSidebar.css";
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
 
 const MenuSidebar = () => {
   return (
@@ -12,7 +13,11 @@ const MenuSidebar = () => {
         <li>WOMEN</li>
         <li>SALE</li>
         <input className="se" type="text" placeholder="Search"></input>
-        
+        <li>
+          <Link to="/store" className="nav-link">
+        <Button  endIcon={<ShoppingBagOutlinedIcon />}> </Button>
+        </Link>
+          </li>
       </ul>
     </div>
   );
