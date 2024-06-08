89fa31c1e28c906ad15243f431fdfb2361bbb671
Khánh Vũ <152385937+kjnhs@users.noreply.github.com>
2024-06-08 20:10:45 +0700

    Revert "Namkhanh"

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -2,28 +2,12 @@ import React from "react";
 import Banner from "../layouts/banner/Banner";
 import Container from "../container/Container";
 import "./HomePage.css";
-import { Card } from "@mui/material";
-import Matching from "../layouts/banner/Matching";
 
 const HomePage = () => {
   return (
     <div className="homepage">
       <Banner />
-      <Matching/>
-      {/* <Card color="neutral" size="md" variant="outlined" sx={{ width: 320 }}>
-        <div
-          className="card"
-          style={{
-            backgroundImage: `url('https://th.bing.com/th/id/OIP.zv2pabAJcqCKkvY5mA8FpgAAAA?pid=ImgDet&w=254&h=169.33333333333334&c=7')`,
-          }}
-        >
-          <div className="card-content">
-            <h2>Galaxy Buds 2019</h2>
-            <p>Perfect for everyone</p>
-          </div>
-        </div>
-      </Card> */}
-      <Container />
+      <Container/>
     </div>
   );
 };
