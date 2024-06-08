a0cf4d7c69de60acbbe73b54d2668a4aaf603462
quanghung306 <94045688+quanghung306@users.noreply.github.com>
2024-06-08 20:00:56 +0700

    commit

--- src/page/StorePage.jsx
+++ src/page/StorePage.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./StorePage.css";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
-import Button from "@mui/material/Button";
+
 import { addToCart, clearCart, decreaseCart, removeFromCart,getTotals } from "../stores/slice/cartSlice";
+import { Button } from "@mui/material";
 
 const StorePage = () => {
   const cart = useSelector((state) => state.cart);
   const dispatch =useDispatch();
+  
+  useEffect(() => {
+    dispatch(getTotals());
+  }, [cart,dispatch]);
+  
 
   const handleDecrement = (cartItem) => {
     dispatch(decreaseCart(cartItem));
@@ -25,78 +30,101 @@ const StorePage = () => {
     dispatch(clearCart(cartItem));
   };
   return (
-    <div className="storepage">
-      <div className="bag">
-        <h4>Bag</h4>
+      <div className="cart-container">
+        <h2>Shopping Cart</h2>
         {cart.cartItems.length === 0 ? (
           <div className="cart-empty">
             <p>Your cart is currently empty</p>
             <div className="start-shopping">
               <Link to="/">
-              <ArrowLeftIcon />
-              <span>Start shopping</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  className="bi bi-arrow-left"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+                  />
+                </svg>
+                <span>Start Shopping</span>
               </Link>
             </div>
           </div>
         ) : (
-          <div className="product-details">
-            <table aria-label="basic table">
-              <thead>
-                <tr>
-                  <th>Product</th>
-                  <th>Name</th>
-                  <th>Size</th>
-                  <th>Quantity</th>
-                  <th>Total Price</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {cart.cartItems.map((cartItem) => (
-                  <tr key={cartItem.id}>
-                    <td>
-                      <img src={cartItem.image} alt={cartItem.title} style={{ width: '50px' }} />
-                    </td>
-                    <td>{cartItem.title}</td>
-                    <td>{cartItem.size}</td>
-                    <td>
-                      <button onClick={() => handleDecrement(cartItem.id)}>-</button>
-                      {cartItem.quantity}
-                      <button onClick={() => handleIncrement(cartItem.id)}>+</button>
-                    </td>
-                    <td>${(cartItem.price * cartItem.quantity).toFixed(2)}</td>
-                    <td>
-                      <button onClick={() => handleRemove(cartItem.id)}>X</button>
-                    </td>
-                  </tr>
+          <div>
+            <div className="titles">
+              <h3 className="product-title">Product</h3>
+              <h3 className="price">Price</h3>
+              <h3 className="quantity">Quantity</h3>
+              <h3 className="total">Total</h3>
+            </div>
+            <div className="cart-items">
+              {cart.cartItems &&
+                cart.cartItems.map((cartItem) => (
+                  <div className="cart-item" key={cartItem.id}>
+                    <div className="cart-product">
+                      <img src={cartItem.image} alt={cartItem.name} />
+                      <div>
+                        <h3>{cartItem.name}</h3>
+                        <p>{cartItem.desc}</p>
+                        <button onClick={() => handleRemove(cartItem)}>
+                          Remove
+                        </button>
+                      </div>
+                    </div>
+                    <div className="cart-product-price">${cartItem.price}</div>
+                    <div className="cart-product-quantity">
+                      <Button onClick={() => handleDecrement(cartItem)}>
+                        -
+                      </Button>
+                      <div className="count">{cartItem.cartQuantity}</div>
+                      <Button onClick={() => handleIncrement(cartItem)}>+</Button>
+                    </div>
+                    <div className="cart-product-total-price">
+                      ${cartItem.price * cartItem.cartQuantity}
+                    </div>
+                  </div>
                 ))}
-              </tbody>
-            </table>
+            </div>
+            <div className="cart-summary">
+              <button className="clear-btn" onClick={() => handleClear()}>
+                Clear Cart
+              </button>
+              <div className="cart-checkout">
+                <div className="subtotal">
+                  <span>Subtotal</span>
+                  <span className="amount">${cart.cartTotalAmount}</span>
+                </div>
+                <p>Taxes and shipping calculated at checkout</p>
+                <button>Check out</button>
+                <div className="continue-shopping">
+                  <Link to="/">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-arrow-left"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+                      />
+                    </svg>
+                    <span>Continue Shopping</span>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         )}
       </div>
-      <div className="Summary">
-        <h4>Summary</h4>
-        <div className="Summary-item">
-          <div className="item">
-            <h6>Subtotal</h6>
-            <span>{cart.cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0)}₫</span>
-          </div>
-          <div className="item">
-            <h6>Estimated Delivery & Handling</h6>
-            <span></span>
-          </div>
-          <div className="item">
-            <h6>Total</h6>
-            <span>{cart.cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0) + 250000}₫</span>
-          </div>
-          <Button variant="contained">Checkout</Button>
-          <ArrowLeftIcon />
-                <span>Start shopping</span>
-        </div>
-      </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default StorePage;
